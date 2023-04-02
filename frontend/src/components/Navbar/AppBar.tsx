fc4f791e62639594ca314b9268d674c0764a8351
Radeonares32 <bugra.linux.js@gmail.com>
2023-04-03 01:52:08 +0300

    sidebar & banner created

--- frontend/src/components/Navbar/AppBar.tsx
+++ frontend/src/components/Navbar/AppBar.tsx
@@ -7,24 +7,30 @@ import { Person,Bell,Gear } from 'react-bootstrap-icons';
 export const AppBar = () => {
     return (
         <>
-        <Navbar collapseOnSelect expand="lg" style={{backgroundColor:'#341e36'}} variant="dark">
+        <Navbar collapseOnSelect expand="lg" style={{backgroundColor:'#fff'}} variant="dark">
                 <Container fluid>
-                <Navbar.Brand className='' href="#home">Logo</Navbar.Brand>
+                <Navbar.Brand style={{color:'black'}} href="#home"><img
+              src="/img/logo/logo.png"
+              width="100"
+              height="70"
+              className="d-inline-block align-top"
+              alt="React Bootstrap logo"
+            /></Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="me-auto">
 
-                            <Nav.Link href="#action1" style={{ marginLeft: '3rem' }}>Canlı Veri</Nav.Link>
+                            <Nav.Link href="#action1" style={{ marginLeft: '3rem',color:"black" }}>Canlı Veri</Nav.Link>
                             
-                            <Nav.Link href="#action2" style={{ marginLeft: '45rem' }}>Canlı Veri</Nav.Link>
+                            <Nav.Link href="#action2" style={{ marginLeft: '45rem',color:"black" }}>Canlı Veri</Nav.Link>
                         
 
-                            <Nav.Link href="#action2" style={{ marginLeft: '40rem' }}>Canlı Veri</Nav.Link>
+                            <Nav.Link href="#action2" style={{ marginLeft: '40rem',color:"black" }}>Canlı Veri</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
-            <Navbar collapseOnSelect expand="lg" style={{backgroundColor:'#341e36'}} variant="dark">
+            <Navbar collapseOnSelect expand="lg" style={{backgroundColor:'#fff'}} variant="dark">
                 <Container fluid>
                    
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
@@ -33,7 +39,7 @@ export const AppBar = () => {
                             <Nav.Link href="#action2" style={{ marginLeft: '57rem',width:'22rem' }}>
                                 <Form className="d-flex">
                                     <Form.Control
-                                        style={{backgroundColor:'#341e36',border:'0px'}}
+                                        style={{backgroundColor:'#fff',border:'2px solid blue',borderRadius:'20px'}}
                                         type="search"
                                         placeholder="Search"
                                         className="me-2"
@@ -43,33 +49,30 @@ export const AppBar = () => {
                             </Nav.Link>
                         </Nav>
                         <Nav>
-                            <Nav.Link href="#deets"><Person  size={34} /></Nav.Link>
+                            <Nav.Link href="#deets"><Person  size={34} color='blue' /></Nav.Link>
                             <Nav.Link  href="#memes">
-                            <Bell size={31} />
+                            <Bell size={31} color='blue' />
                             </Nav.Link>
                             <Nav.Link  href="#memes">
-                            <Gear size={31} />
+                            <Gear size={31} color='blue' />
                             </Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
             
-            <Navbar collapseOnSelect expand="lg" style={{backgroundColor:'#341e36'}} variant="dark">
+            <Navbar collapseOnSelect expand="lg" style={{backgroundColor:'#fff'}} variant="dark">
                 <Container fluid>
 
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="me-auto">
-
-                            <Nav.Link href="#action1" style={{ marginLeft: '1rem' }}>süslü</Nav.Link>
-                            <Nav.Link href="#action2" style={{ marginLeft: '1rem' }}>konudışı</Nav.Link>
-                            <Nav.Link href="#action2" style={{ marginLeft: '1rem' }}>soru-cevap</Nav.Link>
-                            <Nav.Link href="#action2" style={{ marginLeft: '30rem' }}>süslü akış</Nav.Link>
-                            <Nav.Link href="#action2" style={{ marginLeft: '5rem' }}>konu dışı akış</Nav.Link>
-
-                            <Nav.Link href="#action2" style={{ marginLeft: '5rem' }}>soru cevap</Nav.Link>
-                            <Nav.Link href="#action2" style={{ marginLeft: '5rem' }}>medya akış</Nav.Link>
+                            <Nav.Link href="#action2" style={{ marginLeft: '40rem',color:'blue' }}>X1</Nav.Link>
+                            <Nav.Link href="#action2" style={{ marginLeft: '7rem',color:'blue' }}>X2</Nav.Link>
+                            <Nav.Link href="#action2" style={{ marginLeft: '7rem',color:'blue' }}>X3</Nav.Link>
+                            <Nav.Link href="#action2" style={{ marginLeft: '7rem',color:'blue' }}>X4</Nav.Link>
+                            <Nav.Link href="#action2" style={{ marginLeft: '7rem',color:'blue' }}>X5</Nav.Link>
+                            <Nav.Link href="#action2" style={{ marginLeft: '7rem',color:'blue' }}>X6</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
