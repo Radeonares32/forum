521458fe4d3095472cfdbf3cadc554b2d988bd00
Radeonares32 <bugra.linux.js@gmail.com>
2023-04-17 18:15:45 +0300

    main server file created

---
+++ backend/src/server.ts
@@ -0,0 +1,36 @@
+import express from 'express'
+import http from 'http'
+import bodyParser from 'body-parser'
+import session from 'express-session'
+import cors from 'cors'
+
+
+const app = express()
+export const server = http.createServer(app)
+
+
+
+//! Routes
+//import { userRoutes } from './routes/routes'
+
+
+
+app.use(session({
+    secret: 'radeonares',
+    resave: false,
+    saveUninitialized: true
+}))
+
+app.use(cors({
+    origin:"*",
+    allowedHeaders:"*"
+}))
+
+app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+//app.use('/', userRoutes)
+
+if (process.env.NODE_ENV !== 'test') {
+    server.listen(3000)
+}
