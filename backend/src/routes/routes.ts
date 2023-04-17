a10b19a236715c52f3693a7c33c0c98292934269
Radeonares32 <bugra.linux.js@gmail.com>
2023-04-17 21:42:33 +0300

    route refactor

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -1,1 +1,33 @@
-export * as userRoute from "./user.route";
+import express from "express";
+
+const app = express.Router();
+
+//Routes
+import {
+  deleteUser,
+  followUser,
+  getFollowUser,
+  getFollowerUser,
+  getUser,
+  getUserId,
+  logoutUser,
+  postUser,
+  putUser,
+  signUser,
+  unFollowUser,
+} from "../routes/user.route";
+
+export const userRoute = app.use(
+  "/user",
+  deleteUser,
+  followUser,
+  getFollowUser,
+  getFollowerUser,
+  getUser,
+  getUserId,
+  logoutUser,
+  postUser,
+  putUser,
+  signUser,
+  unFollowUser
+)
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,36 +1,35 @@
-import express from 'express'
-import http from 'http'
-import bodyParser from 'body-parser'
-import session from 'express-session'
-import cors from 'cors'
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import session from "express-session";
+import cors from "cors";
 
-
-const app = express()
-export const server = http.createServer(app)
-
-
+const app = express();
+export const server = http.createServer(app);
 
 //! Routes
-//import { userRoutes } from './routes/routes'
+import { userRoute } from "./routes/routes";
 
-
-
-app.use(session({
-    secret: 'radeonares',
+app.use(
+  session({
+    secret: "radeonares",
     resave: false,
-    saveUninitialized: true
-}))
+    saveUninitialized: true,
+  })
+);
 
-app.use(cors({
-    origin:"*",
-    allowedHeaders:"*"
-}))
+app.use(
+  cors({
+    origin: "*",
+    allowedHeaders: "*",
+  })
+);
 
-app.use(express.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-//app.use('/', userRoutes)
+app.use("/", userRoute);
 
-if (process.env.NODE_ENV !== 'test') {
-    server.listen(3000)
+if (process.env.NODE_ENV !== "test") {
+  server.listen(3000);
 }
